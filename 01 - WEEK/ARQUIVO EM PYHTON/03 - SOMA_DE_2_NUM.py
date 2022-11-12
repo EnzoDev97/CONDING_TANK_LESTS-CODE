a98b28ff0b8effa95a96e5b2dd702d52a5497471
Enzo <90116258+EnzoDev97@users.noreply.github.com>
2022-11-11 22:58:41 -0300

    Update 03 - SOMA_DE_2_NUM.py

--- 01 - WEEK/ARQUIVO EM PYHTON/03 - SOMA_DE_2_NUM.py
+++ 01 - WEEK/ARQUIVO EM PYHTON/03 - SOMA_DE_2_NUM.py
@@ -1,55 +1,7 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 1,
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "----------------------------------------------------------------------------------------------------\n",
-      "10 + 7 = 17 \n"
-     ]
-    }
-   ],
-   "source": [
-    "# 01 - SOMANDO DOIS NÚMEROS REAIS.\n",
-    "\n",
-    "num_1 = int(input('DIGITE O PRIMEIRO NÚMERO: '))\n",
-    "num_2 = int(input('DIGITE O SEGUNDO NÚMERO: '))\n",
-    "print('-'*100)\n",
-    "soma = num_1 + num_2\n",
-    "print('{} + {} = {} '.format(num_1,num_2,soma))"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3.9.12 ('base')",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.9.12"
-  },
-  "orig_nbformat": 4,
-  "vscode": {
-   "interpreter": {
-    "hash": "84119b4a23c727451d2f5b202898a53a27583c1b582daf411d2362849740e029"
-   }
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+# 01 - SOMANDO DOIS NÚMEROS REAIS.
+
+num_1 = int(input('DIGITE O PRIMEIRO NÚMERO: '))
+num_2 = int(input('DIGITE O SEGUNDO NÚMERO: '))
+print('-'*100)
+soma = num_1 + num_2
+print('{} + {} = {} '.format(num_1,num_2,soma))
